b0433d72a258853186b29f2037e795abf0f52b4c
naru-77x <naru.fumi@icloud.com>
2022-01-12 01:45:29 +0900

    css適用

--- index.js
+++ index.js
@@ -3,63 +3,46 @@
  * input form の入力内容を取得してそれをTODOリストに加える
  */
 function handleFormSubmit() {
-    //入力内容の取得
-    const inputEl = document.getElementById("task-input");
-    const inputValue = inputEl.nodeValue;
-
-    //テキストが入力されてない時
-    if (!inputValue.length > 0) {
-        alert("テキストを入力してください。");
-        return;
-    }
-
-      // Todo の追加対象を取得する
+  const input = document.getElementById("task-input");
+  const inputValue = input.value; // 厳密には instanceof で input要素であるかのチェックを入れるべき
+  if (!inputValue.length > 0) {
+    alert("テキストを入力してください。");
+    return;
+  }
   const todosEl = document.getElementById("todos");
-
-  // TODO要素を作る (チェックボックス・ラベル・削除ボタンのセット)
   const todoEl = createTodoElement(inputValue);
-
-  // TODOの表示エリアにTODOを追加
   todosEl.appendChild(todoEl);
-
-  // input のリセット
-  input.value = "";
+  input.value = ""; // input のリセット
 }
 
 /**
  * TODO要素を作る関数
+ * @param {*} inputValue TODO文字列
+ * @returns TODO要素
  */
-function createTodoElement(task) {
-    // TODO要素を作る
-    const todoEl = document.createElement("li");
-  
-    // checkbox要素を作る
-    const checkBoxEl = document.createElement("input");
-    checkBoxEl.type = "checkbox";
-    checkBoxEl.onchange = function (e) {
-      const checked = e.target.checked;
-      if (checked) {
-        todoEl.className = `checked`;
-      } else {
-        todoEl.className = "";
-      }
-    };
-  
-    // label要素を作る
-    const labelEl = document.createElement("label");
-    labelEl.innerText = task;
-  
-    // ボタン要素を作る
-    const buttonEl = document.createElement("button");
-    buttonEl.innerText = "削除";
-    buttonEl.onclick = function () {
-      todoEl.remove();
-    };
-  
-    // checkbox, label, button を TODO要素にセットする
-    todoEl.appendChild(checkBoxEl);
-    todoEl.appendChild(labelEl);
-    todoEl.appendChild(buttonEl);
-  
-    return todoEl;
-  }
+function createTodoElement(inputValue) {
+  const todoEl = document.createElement("li");
+  const checkBoxEl = document.createElement("input");
+  todoEl.appendChild(checkBoxEl);
+  const labelEl = document.createElement("label");
+  labelEl.innerText = inputValue;
+  checkBoxEl.type = "checkbox";
+  checkBoxEl.onchange = function (e) {
+    const checked = e.target.checked;
+    if (checked) {
+      todoEl.className = `checked`;
+    } else {
+      todoEl.className = "";
+    }
+  };
+  todoEl.appendChild(labelEl);
+
+  const buttonEl = document.createElement("button");
+  buttonEl.innerText = "削除";
+  buttonEl.onclick = function () {
+    todoEl.remove();
+  };
+  todoEl.appendChild(buttonEl);
+
+  return todoEl;
+}
